fa1eeec2a2d79261c3516f008741e38b43ac714e
DESKTOP-P6EHM7V\Ilham zaki <ilhamzaki32@gmail.com>
2021-01-31 14:26:20 +0700

    Updating asmaul husna feature , add skeleton asmaul card

--- src/components/featureAsmaulHusna/FeatureAsmaulHusna.jsx
+++ src/components/featureAsmaulHusna/FeatureAsmaulHusna.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 import { connect } from "react-redux";
 import "./FeatureAsmaulHusna.scss";
+import FeatureAsmaulSkeleton from "../featureAsmaulSkeleton/FeatureAsmaulSkeleton";
 
 const mapStateToProps = (state) => {
   return {
@@ -15,28 +16,32 @@ const mapStateToProps = (state) => {
 const FeatureAsmaulHusna = (props) => {
   console.log(props);
   const { items, loading, error } = props;
-  if(loading) return <h1>Loadding....</h1>;
-  if(error) return <h1>Error</h1>
-  
+  if (error) return <h1>Error</h1>;
+
   return (
-      <section id="featured-asmaulhusna">
-        <Container>
-          <Grid container spacing={3}>
-            {items.map((item) => {
-              console.log(item);
-              return (
-                <Grid item sm={6} md={4} xs={12} spacing={3}>
+    <section id="featured-asmaulhusna">
+      <Container>
+        <Grid container spacing={3}>
+          {loading ? (
+            <>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((value) => (
+                <FeatureAsmaulSkeleton key={value} />
+              ))}
+            </>
+          ) : (
+            items.map((item) => (
+              <Grid item sm={6} md={4} xs={12} spacing={3}>
                 <div className="asmaul-card">
-                    <h2 className="text">{item.name}</h2>
-                    <p className="textTransliteration">{`${item.number}. ${item.transliteration}`}</p>
-                    <p className="textTranslation">{item.en.meaning}</p>
+                  <h2 className="text">{item.name}</h2>
+                  <p className="textTransliteration">{`${item.number}. ${item.transliteration}`}</p>
+                  <p className="textTranslation">{item.en.meaning}</p>
                 </div>
               </Grid>
-              );
-            })}
-          </Grid>
-        </Container>
-      </section>
+            ))
+          )}
+        </Grid>
+      </Container>
+    </section>
   );
 };
 
