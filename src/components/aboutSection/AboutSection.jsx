e9efc2901988820e2fc71cc9836a35beeaa2ed47
DESKTOP-RIGDD5R\minrn <ikhlas.10119022@mahasiswa.unikom.ac.id>
2021-02-05 00:57:58 +0700

    updating homepage aboutsection, change background design and title al-quran at about section component

--- src/components/aboutSection/AboutSection.jsx
+++ src/components/aboutSection/AboutSection.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./AboutSection.scss";
+import alquranTitle from "../../assets/images/alquranTitle.png";
 const AboutSection = () => {
   return (
     <section id="about">
-      <div className="inner">
-        <p>OurQur'an</p>
-        <h1>Al-Qur'an</h1>
-        <p>
-          “This is the book about which there <br />
-          is no doubt, A guidance for those <br />
-          conscious of Allah”
-        </p>
+      <div id="about-desktop">
+        <div className="inner">
+          <p>OurQur'an</p>
+          <img src={alquranTitle} alt="alquran-title" />
+          <p>
+            “This is the book about which there <br />
+            is no doubt, A guidance for those <br />
+            conscious of Allah”
+          </p>
+        </div>
       </div>
     </section>
   );
