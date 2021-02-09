cd90880ad0b518f4aa92709250d9ad73a31338fa
DESKTOP-RIGDD5R\minrn <ikhlas.10119022@mahasiswa.unikom.ac.id>
2021-02-09 14:58:11 +0700

    updating NoMatch Page, add responsive ipad

--- src/pages/noMatch/NoMatch.jsx
+++ src/pages/noMatch/NoMatch.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@material-ui/core";
+import { Hidden } from "@material-ui/core";
 import noMatchBackground from "../../assets/images/notMatchBackground.svg";
+import noMatchBackgroundipad from "../../assets/images/notMatchBackgroundipad.svg";
+import noMatchBackgroundmobile from "../../assets/images/notMatchBackgroundmobile.svg";
+
 import "./NoMatch.scss";
 class NoMatch extends Component {
   render() {
     return (
       <section id="no-match" className="top-page">
-        <div className="background">
-          <img src={noMatchBackground} alt="no-match" />
-        </div>
+        <Hidden mdDown>
+          <div className="background-desktop">
+            <img src={noMatchBackground} alt="no-match" />
+          </div>
+        </Hidden>
+        <Hidden xsDown lgUp>
+          <div className="background-ipad">
+            <img src={noMatchBackgroundipad} alt="no-match" />
+          </div>
+        </Hidden>
+
         <div id="title">
           <div className="title-box">
             <h1>Ooops..</h1>
